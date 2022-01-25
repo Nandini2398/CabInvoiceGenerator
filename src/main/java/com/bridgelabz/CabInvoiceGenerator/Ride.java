d9cbff59811423a5cfbb117d7cfc18ccbced24fb
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 12:56:06 +0530

    UPDATED:fare fofr multiple rides

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.CabInvoiceGenerator;
+
+public class Ride {
+		
+		public double distance;
+		public int time;
+		
+		Ride(double distance, int time){
+			this.distance = distance;
+			this.time = time;
+		}
+	}
