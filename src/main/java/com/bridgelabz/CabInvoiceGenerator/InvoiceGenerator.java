744d55c3654b002425c4834fe67b06c5003a953e
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 14:22:16 +0530

    UPDATED:enhanced invoice of fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/InvoiceGenerator.java
@@ -5,16 +5,27 @@ public class InvoiceGenerator {
 	private static final double MIN_COSY_PER_KM = 10;
 	private static final int MIN_COST_PER_MINUTE = 1;
 	private static final double MIN_CAB_FARE = 5;
+	
+
 	public double totalFare(double distance, int time) {
 		double fare = distance *MIN_COSY_PER_KM + time * MIN_COST_PER_MINUTE;
 		double totalCabFare = Math.max(MIN_CAB_FARE, fare);
 		return totalCabFare;
 	}
 	public double calculateFare(Ride[] rides) {
+		
 		double totalFare = 0;
 		for(Ride ride : rides) {
 			totalFare = totalFare + this.totalFare(ride.distance, ride.time);
 		}
 		return totalFare;
+	}
+	public InvoiceSummary calculateFareReturnObject(Ride[] rides) {
+		
+		double totalFare = 0;
+		for(Ride ride : rides) {
+			totalFare = totalFare + this.totalFare(ride.distance, ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 }
