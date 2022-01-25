bc0999c0f8883129fbb0c189867651302db48b00
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 14:18:09 +0530

    UPDATED:test case for fare of multiple rides

--- src/test/java/com/bridgelabz/CabInvoiceGenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGenerator/InvoiceGeneratorTest.java
@@ -2,25 +2,37 @@ package com.bridgelabz.CabInvoiceGenerator;
 
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
-	
-@Test
-public void whenDistanceAndTimeGiven_ShouldReturnTotalFare() {
-			InvoiceGenerator generator = new InvoiceGenerator();
-			double distance=1;
-			int time = 6;
-			double fare = generator.totalFare(distance,time);
-			assertEquals(16,fare,0.0);
-		}
-@Test
-public void whenDistanceAndTimeGiven_WhenTotalFareLessThan5_ShouldReturnMinFare() {
-			InvoiceGenerator generator = new InvoiceGenerator();
-			double distance=0.1;
-			int time = 2;
-			double fare = generator.totalFare(distance,time);
-			assertEquals(5,fare,0.0);
-		}
-	}
+	InvoiceGenerator generator = null;
+	@Before
+	public void setUp() {
+		generator = new InvoiceGenerator();
 
+	}
+	@Test
+	public void whenDistanceAndTimeGiven_ShouldReturnTotalFare() {
+		double distance=1;
+		int time = 6;
+		double fare = generator.totalFare(distance,time);
+		assertEquals(16,fare,0.0);
+	}
+	@Test
+	public void whenDistanceAndTimeGiven_WhenTotalFareLessThan5_ShouldReturnMinFare() {
+		double distance=0.1;
+		int time = 2;
+		double fare = generator.totalFare(distance,time);
+		assertEquals(5,fare,0.0);
+	}
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+
+		Ride[] rides = {new Ride(2.0, 5), 
+						new Ride(0.1, 1)};
+		double totalFare = generator.calculateFare(rides);
+		assertEquals(30, totalFare, 0.0);
+	}
+}
