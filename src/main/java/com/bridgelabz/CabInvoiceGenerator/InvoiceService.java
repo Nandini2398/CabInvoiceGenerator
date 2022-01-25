1203514eb3e5bb1b286d6e36f3056aa376519028
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-25 14:34:47 +0530

    UPDATED:enhanced invoice with user id

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/InvoiceService.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/InvoiceService.java
@@ -1,25 +1,23 @@
 package com.bridgelabz.CabInvoiceGenerator;
 
-	import java.util.ArrayList;
-	import java.util.HashMap;
-	import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-	public class InvoiceService {
-		
-		Map<String, ArrayList<Ride>> rideMap;
-		
-		public InvoiceService() {
-			this.rideMap= new HashMap<>();
-		}
-		
-		public void addRide(String customerId, ArrayList<Ride> rides) {
-			if(customerId != null)
-				rideMap.put(customerId, rides);
-		}
-		public ArrayList<Ride> getRides(String customerId){
-			if(rideMap.containsKey(customerId))
-				return rideMap.get(customerId);
-			return null;
-		}
+public class InvoiceService {
+	
+	Map<String, ArrayList<Ride>> rideMap;
+	public InvoiceService() {
+		this.rideMap= new HashMap<>();
 	}
+	public void addRide(String customerId, ArrayList<Ride> rides) {
+		if(customerId != null)
+			rideMap.put(customerId, rides);
+	}
+	public ArrayList<Ride> getRides(String customerId){
+		if(rideMap.containsKey(customerId))
+			return rideMap.get(customerId);
+		return null;
+	}
+}
 
